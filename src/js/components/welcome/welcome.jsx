d4f006eff081877dbbee5cfd072bc54f7a0849b3
james <jameswestwood@protonmail.com>
2018-07-04 18:27:29 +1000

    add react-strap

--- src/js/components/welcome/welcome.jsx
+++ src/js/components/welcome/welcome.jsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
-import Button from '../button/button.jsx';
+import { Button } from 'reactstrap';
 
 import styles from '../../../css/content.css';
 
@@ -17,8 +17,8 @@ class Welcome extends React.Component<Props>
         <h2>Welcome</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex <a href="#">ea commodo consequat</a>.</p>
-        <Button label="primary" type="cta" />
-        <Button label="secondary" />
+
+        <Button color="danger">Danger!</Button>
       </div>
     );
   }
